a43081101d061e7e9d28161589b152f3882262cb
Vrajendra <vrajendra.bhavsar@mindinventory.com>
2022-12-28 15:27:35 +0530

    - added live data

--- app/src/main/java/com/example/propaintcontainerscreen/presentation/mypaint/MyPaintsViewModel.kt
+++ app/src/main/java/com/example/propaintcontainerscreen/presentation/mypaint/MyPaintsViewModel.kt
@@ -2,6 +2,8 @@ package com.example.propaintcontainerscreen.presentation.mypaint
 
 import android.content.Context
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.propaintcontainerscreen.data.PaintContainer
 import com.google.firebase.crashlytics.buildtools.reloc.com.google.common.reflect.TypeToken
@@ -9,6 +11,12 @@ import com.google.gson.Gson
 import java.io.IOException
 
 class MyPaintsViewModel: ViewModel() {
+
+    /*private val _containerAmount = MutableLiveData<Int>(0)
+    val containerAmount : LiveData<Int> = _containerAmount
+
+    private val _itemQuantity = MutableLiveData<Int>(1)
+    val itemQuantity : LiveData<Int> = _itemQuantity*/
 
     fun getContainerData(context: Context): List<PaintContainer> {
         lateinit var jsonString: String
